43c7a7790db2e86d798c56ed14b85bd49ad901f8
D4Saki <thkvtpa@gmail.com>
2026-04-30 16:23:03 +0700

    God damm fix Red

--- Assets/Script/Projectile2D.cs
+++ Assets/Script/Projectile2D.cs
@@ -21,21 +21,21 @@ public class Projectile2D : MonoBehaviour
                 Debug.Log("hit " + hit.collider.name);
 
                 Vector2 projectileVelocity = CalculateProjectileVelocity(shootPoint.position, hit.point, 1f);
-                Rigidbody2D shootBullet = Instantiate(bulletPrefab, shootPoint.position, Quaternion.identity);
+
+                Rigidbody2D shootBullet = Instantiate(bulletPrefab, shootPoint.position, Quaternion.identity).GetComponent<Rigidbody2D>();
+
                 shootBullet.linearVelocity = projectileVelocity;
             }
-            Vector2 CalculateProjectileVelocity(Vector2 origin, Vector2 target, float time)
-            {
-                Vector2 distance = target - origin;
-                float velocityX = distance.x / time;
-                float velocityY = distance.y / time + 0.5f * Mathf.Abs(Physics2D.gravity.y) * time;
+        }
+        Vector2 CalculateProjectileVelocity(Vector2 origin, Vector2 target, float time)
+        {
+            Vector2 distance = target - origin;
+            float velocityX = distance.x / time;
+            float velocityY = distance.y / time + 0.5f * Mathf.Abs(Physics2D.gravity.y) * time;
 
-                Vector2 projectileVelocity = new Vector2(velocityX, velocityY);
+            Vector2 projectileVelocity = new Vector2(velocityX, velocityY);
 
-                return projectileVelocity;
-            }
+            return projectileVelocity;
         }
     }
-    
-     
 }
